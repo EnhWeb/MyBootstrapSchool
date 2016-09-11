25b62f56bd5eb27e216d77d9d23c8cc06a935918
263646469@qq.com <263646469@qq.com>
2016-09-11 17:05:17 +0800

    123123

---
+++ MyBootstrapSchool/Areas/Manage/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBootstrapSchool.Areas.Manage.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Manage/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
