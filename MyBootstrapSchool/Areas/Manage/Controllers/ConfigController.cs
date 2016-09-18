124fcb311bcc7bcd5d35a8ad22033e97e0d22e53
263646469@qq.com <263646469@qq.com>
2016-09-18 10:15:28 +0800

    更新

---
+++ MyBootstrapSchool/Areas/Manage/Controllers/ConfigController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBootstrapSchool.Areas.Manage.Controllers
+{
+    public class ConfigController : Controller
+    {
+        // GET: Manage/Config
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string SkinName)
+        {
+
+                //Response.Write("ace.settings 为空");
+
+            HttpCookie cookie = new HttpCookie("ace.SkinId", string.Format(" class=\"{0}\"", SkinName)) { Expires = DateTime.Now.AddDays(300) };
+                Response.Cookies.Add(cookie);
+
+            var result = new {
+                data = "ok",
+                status = "success",
+            };
+
+            return Json(result, "application/json", System.Text.Encoding.UTF8, JsonRequestBehavior.DenyGet);
+
+        }
+    }
+}
